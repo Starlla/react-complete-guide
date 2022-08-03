afbcafffc4ed16d207ac1e3595aa5e113fe6c444
Claire <claire.cy.t@gmail.com>
2022-08-03 17:23:06 -0400

    format date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItems({ title, amount, date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { year: "2-digit" });
+  const year = date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <div>
           <h2>{title}</h2>
