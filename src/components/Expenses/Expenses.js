6009151d02618c7b99b294cf440911c098f1db27
Claire <claire.cy.t@gmail.com>
2022-08-15 23:07:30 -0400

    add year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,15 @@ import React from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses({ expenses }) {
+  const selectChangeHandler = (selectedYear) => {
+    console.log(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter onSelectChange={selectChangeHandler} />
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.title}
